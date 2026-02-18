72f0be26bc3cd444018b27d9b305e174621be9ea
LucaryDev <242092893+LucaryDev@users.noreply.github.com>
2026-02-17 21:17:19 -0300

    feat(js): adiciona classe Usuario com geração automática de id

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ const nome = document.getElementById("nome")
 const email = document.getElementById("email")
 
 const senha = document.getElementById("senha")
+
+// JS
+let contadorId = 0
+
+class Usuario {
+  constructor (nome,email,senha) {
+    contadorId++
+    this.id = contadorId
+    this.nome = nome
+    this.email = email
+    this.senha = senha
+  }
+}
